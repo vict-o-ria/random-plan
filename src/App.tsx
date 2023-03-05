2038f96ca38cdb9344fad8af84ed1eb50478e315
Victoria Salova <salovavika@gmail.com>
2023-03-05 18:30:08 +0600

    add ofline indicator

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,38 @@ import { RandomTask } from 'pages/RandomTask';
 import { List } from 'pages/List';
 import { Menu } from 'components/Menu';
 import { ErrorBoundary } from 'components/ErrorBoundary';
+import { useEffect, useState } from 'react';
 
-export const App = () => (
-  <BrowserRouter>
-    <ErrorBoundary>
-    <Menu />
-    <Routes>
-      <Route index element={<RandomTask />} />
-      <Route path="/list" element={<List />} />
-    </Routes>
-    </ErrorBoundary>
-  </BrowserRouter>
-);
+export const App = () => {
+  const [offline, setOffline] = useState(false);
+
+  useEffect(() => {
+    const offlineListener = () => {
+      if (navigator.onLine) {
+        setOffline(false);
+      } else {
+        setOffline(true);
+      }
+    };
+    window.addEventListener('offline', offlineListener);
+    return () => {
+      window.removeEventListener('offline', offlineListener);
+    };
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <ErrorBoundary>
+        <Menu />
+        {offline ? (
+          <h1>App is offline. Check your connection</h1>
+        ) : (
+          <Routes>
+            <Route index element={<RandomTask />} />
+            <Route path="/list" element={<List />} />
+          </Routes>
+        )}
+      </ErrorBoundary>
+    </BrowserRouter>
+  );
+};
